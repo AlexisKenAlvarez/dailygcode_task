4bd5ead1b436dc2552e7f0573f39f1a3f3b04fc0
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-11-29 10:38:38 +0800

    Fixed focusing

--- src/components/InputModal.js
+++ src/components/InputModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import TextareaAutosize from 'react-textarea-autosize';
 import { useSelector } from 'react-redux';
 
@@ -8,6 +8,7 @@ export const InputModal = ({label, className, id, mLeft}) => {
     const itemList = useSelector((state) => state.listData)
     const [name, setName] = useState('')
     const [value, setValue] = useState('')
+    const inputRef = useRef(null)
 
     const handleChange = (e) => {
         setValue(e.target.value)
@@ -17,10 +18,14 @@ export const InputModal = ({label, className, id, mLeft}) => {
       setName(id)
 
     }, [])
+
+    const handleFocus = () => {
+        inputRef.current.focus()
+    }
     
 
     return (
-        <div className={`input-container mx-auto max-w-[800px] border-0 border-black ${className}`}>
+        <div className={`input-container mx-auto max-w-[800px] border-0 border-black ${className}`} onClick={handleFocus}>
             <div className='flex w-full h-auto items-center'>
                 <p className="text-md font-sans w-[25rem]">{label}</p>
                 <div className={`relative flex items-center w-[20px] h-4 ml-auto mr-2 sm:mr-auto ${mLeft}`}>
@@ -30,7 +35,7 @@ export const InputModal = ({label, className, id, mLeft}) => {
             <div className="w-full h-auto rounded transition-all ease-in-out duration-200 mt-3 focus-within:outline focus-within:outline-focusBorder focus-within:outline-3">
                 <div className="group w-full h-auto overflow-hidden border-[1px] rounded border-bOutline items-center bg-white relative py-2 pl-2 hover:border-hoverBorder transition-all ease-in-out duration-500 focus-within:border-hoverBorder cursor-text">
                     {/* <textarea className='w-full h-10 m-0 p-0 outline-none' style={{resize: "none"}}></textarea> */}
-                    <TextareaAutosize className='w-full outline-none h-12 m-0 pr-3 pb-4' minRows={1} maxRows={4} style={{resize: "none"}} onChange={handleChange}/>
+                    <TextareaAutosize className='w-full outline-none h-12 m-0 pr-3 pb-4' minRows={1} maxRows={4} style={{resize: "none"}} onChange={handleChange} ref={inputRef}/>
                 </div>
             </div>
         </div>
