c46116e2cf890e233dd8f44d48aaec9a76bb43a0
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-11-29 10:33:28 +0800

    Added ellipsis

--- src/components/Things/List.js
+++ src/components/Things/List.js
@@ -14,9 +14,11 @@ const List = (props) => {
     }
 
     return (
-        <motion.li initial={{scale: 0}} animate={{scale: 1}} exit={{scale: 0}} className="rounded bg-[#fafafa] w-fit h-auto list-none py-[0.1rem] px-2 border-[0.1px] border-[#eaeaea] flex items-center justify-center mr-1 float-left select-none cursor-default">
-            {props.items}
-            <MdClose className="ml-1 mt-[0.1rem] cursor-pointer text-[#898989] hover:text-black transition-all ease-in-out" id={props.items} onClick={handleRemove} />
+        <motion.li initial={{scale: 0}} animate={{scale: 1}} exit={{scale: 0}} className="rounded bg-[#fafafa] w-fit max-w-full h-auto list-none py-[0.1rem] px-2 border-[0.1px] border-[#eaeaea] flex items-center justify-center mr-1 float-left select-none cursor-default relative pr-6">
+            <p className=' truncate text-ellipsis'>
+                {props.items}
+            </p>
+            <MdClose className="absolute ml-1 mt-[0.1rem] cursor-pointer text-[#898989] hover:text-black transition-all ease-in-out right-1" id={props.items} onClick={handleRemove} />
         </motion.li>
     )
 }
